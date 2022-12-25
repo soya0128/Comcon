b359f64907dfe88973012485187eae04ac6a9ebd
soya0128 <bori.rhythem2545@gmail.com>
2022-12-25 22:13:07 +0900

    userモデル単体テストコードの追加
    マイページ実装に伴うもの

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -92,4 +92,76 @@ RSpec.describe User, type: :model do
       end
     end
   end
+
+  describe 'ユーザー情報更新' do
+    context '情報更新ができる場合' do
+      it '全ての値が存在すれば更新できる' do
+        expect(@user).to be_valid
+      end
+    end
+    context '情報更新ができない場合' do
+      it 'ニックネームが空では更新できない' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("ニックネームを入力してください")
+      end
+      it 'Eメールアドレスが空では更新できない' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Eメールを入力してください")
+      end
+      it 'Eメールアドレスに@が含まれないと更新できない' do
+        @user.email = 'user.com'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Eメールは不正な値です")
+      end
+      it '同じEメールアドレスがすでに登録されていると更新できない' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Eメールはすでに存在します")
+      end
+      it '苗字(全角)が空では更新できない' do
+        @user.first_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("苗字を入力してください")
+      end
+      it '苗字(全角)が全角以外では更新できない' do
+        @user.first_name = 'aaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("苗字は不正な値です")
+      end
+      it '名前(全角)が空では更新できない' do
+        @user.last_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("名前を入力してください")
+      end
+      it '名前(全角)が全角以外では更新できない' do
+        @user.last_name = 'bbb'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("名前は不正な値です")
+      end
+      it '苗字(全角カナ)が空では更新できない' do
+        @user.first_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("苗字(カナ)を入力してください")
+      end
+      it '苗字(全角カナ)が全角カタカナ以外では更新できない' do
+        @user.first_name_kana = 'ccc'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("苗字(カナ)は不正な値です")
+      end
+      it '名前(全角カナ)が空では更新できない' do
+        @user.last_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("名前(カナ)を入力してください")
+      end
+      it '名前(全角カナ)が全角カタカナ以外では更新できない' do
+        @user.last_name_kana = 'ddd'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("名前(カナ)は不正な値です")
+      end
+    end
+  end
 end
