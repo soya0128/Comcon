a6dfd3bc45a262bfd92f91c2d5c50db66371f5a5
soya0128 <bori.rhythem2545@gmail.com>
2022-12-29 20:45:22 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root 'users#index'
   resources :users, only: [:show, :edit, :update]
   resources :members, only:[:index, :new, :create, :edit, :update]
-  resources :emails, only:[:index, :new, :create]
+  resources :emails, only:[:index, :new, :create, :show]
 end
