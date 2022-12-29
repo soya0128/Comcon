0a56c92ef80cb3b39ae1b319ec7567040a8960a4
soya0128 <bori.rhythem2545@gmail.com>
2022-12-29 20:27:47 +0900

    emailsコントローラーの編集

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,2 +1,24 @@
 class EmailsController < ApplicationController
+  def index
+    @emails = Email.include(:user)
+  end
+
+  def new
+    @email = Email.new
+  end
+
+  def create
+    @email = Email.new(email_params)
+    if @email.save
+      redirect_to action:'index'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def email_params
+    params.require(:email).permit(:title, :content).merge(user_id: current_user.id)
+  end
 end
