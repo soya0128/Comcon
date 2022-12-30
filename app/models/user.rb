b55fbde84f2a916d2a3cd8d3ba1ab73e0e0c24fc
soya0128 <bori.rhythem2545@gmail.com>
2022-12-30 20:35:03 +0900

    userテーブル関係の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :email, uniqueness:{ case_sensitive: false}
   validates :nickname, presence: true
-  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
 
   has_one :member
   has_many :emails
