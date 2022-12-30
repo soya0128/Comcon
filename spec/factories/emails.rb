7f3cdf5ee7488d769f6fa899c4732de4a24f9b49
soya0128 <bori.rhythem2545@gmail.com>
2022-12-30 18:32:50 +0900

    emailモデル単体テストコードの実装

--- spec/factories/emails.rb
+++ spec/factories/emails.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :email do
-    
+    subject { Faker::Lorem.sentence }
+    content { Faker::Lorem.paragraph }
+    association :user
   end
 end
--- spec/mailers/email_spec.rb
+++ spec/mailers/email_spec.rb
@@ -1,5 +1,27 @@
 require "rails_helper"
 
 RSpec.describe EmailMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @email = FactoryBot.build(:email)
+  end
+
+  describe 'メール新規作成' do
+    context 'メールがDBに保存される場合' do
+      it '件名、本文があれば保存できる' do
+        expect(@email).to be_valid
+      end
+    end
+    context 'メールがDBに保存されない場合' do
+      it '件名が空だと保存できない' do
+        @email.subject = ''
+        @email.valid?
+        expect(@email.errors.full_messages).to include("件名を入力してください")
+      end
+      it '本文が空だと保存できない' do
+        @email.content = ''
+        @email.valid?
+        expect(@email.errors.full_messages).to include("本文を入力してください")
+      end
+    end
+  end
 end
