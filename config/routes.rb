ccbb6abc445aeddfa4a371acf404faf84278973e
soya0128 <bori.rhythem2545@gmail.com>
2022-12-30 21:53:55 +0900

    ルーティングの修正
    membersをネスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
-  resources :users, only: [:show, :edit, :update]
-  resources :members, only:[:index, :new, :create, :edit, :update]
+  resources :users, only: [:show, :edit, :update] do
+    resources :members, only:[:index, :new, :create, :edit, :update]
+  end
   resources :emails, only:[:index, :new, :create, :show]
 end
