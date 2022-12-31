f057b54659c6626a0471f9c2ca066de08d2a54fe
soya0128 <bori.rhythem2545@gmail.com>
2022-12-31 16:13:25 +0900

    memberビューファイルの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,9 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect_to user_path(@user.id)
+    user = User.find(params[:id])
+    if user.update(user_params)
+      redirect_to user_path(user.id)
     else
       render :edit
     end 
