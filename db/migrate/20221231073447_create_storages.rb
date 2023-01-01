7078bb153d057de96a4a273988a6156c501517e8
soya0128 <bori.rhythem2545@gmail.com>
2023-01-01 14:38:41 +0900

    storageモデルの作成、マイグレート

---
+++ db/migrate/20221231073447_create_storages.rb
@@ -0,0 +1,8 @@
+class CreateStorages < ActiveRecord::Migration[6.0]
+  def change
+    create_table :storages do |t|
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_29_092930) do
+ActiveRecord::Schema.define(version: 2022_12_31_073447) do
 
   create_table "emails", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "subject", null: false
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 2022_12_29_092930) do
     t.index ["user_id"], name: "index_members_on_user_id"
   end
 
+  create_table "storages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_storages_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", null: false
     t.string "encrypted_password", null: false
@@ -53,4 +60,5 @@ ActiveRecord::Schema.define(version: 2022_12_29_092930) do
 
   add_foreign_key "emails", "users"
   add_foreign_key "members", "users"
+  add_foreign_key "storages", "users"
 end
