832e42e87992ee1756bb7b8fed1990af9aeefd51
soya0128 <bori.rhythem2545@gmail.com>
2023-01-07 18:42:25 +0900

    storagesコントローラーの記述

--- app/controllers/storages_controller.rb
+++ app/controllers/storages_controller.rb
@@ -3,4 +3,10 @@ class StoragesController < ApplicationController
   def index
     @storages = Storage.includes(:storage).order("created_at DESC")
   end
+
+  def new
+  end
+
+  def create
+  end
 end
