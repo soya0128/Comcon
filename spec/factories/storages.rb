0148b4f1cbc18903cf5c9a7727b261375ad30a97
soya0128 <bori.rhythem2545@gmail.com>
2023-01-09 21:38:11 +0900

    storageモデルの単体テストコードの実装

--- spec/factories/storages.rb
+++ spec/factories/storages.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :storage do
-    
+    name  { Faker::Lorem.sentence }
+    association :user
+
+    after(:build) do |storage|
+      storage.image.attach(io: File.open('public/images/present_box.png'),filename: 'present_box.png')
+    end
   end
 end
