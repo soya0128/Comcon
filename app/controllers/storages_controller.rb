28959d135cc9829f326d5b42dfecea71981abf05
soya0128 <bori.rhythem2545@gmail.com>
2023-01-09 20:37:55 +0900

    storagesコントローラーの編集

--- app/controllers/storages_controller.rb
+++ app/controllers/storages_controller.rb
@@ -1,12 +1,30 @@
 class StoragesController < ApplicationController
 
   def index
-    @storages = Storage.includes(:storage).order("created_at DESC")
+    @storages = Storage.includes(:user).order("created_at DESC")
   end
 
   def new
+    @storage = Storage.new
   end
 
   def create
+    @storage = Storage.new(storage_params)
+    if @storage.save
+      redirect_to storages_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    storage = Storage.find(params[:id])
+    storage.destroy
+    redirect_to storages_path
+  end
+
+  private
+  def storage_params
+    params.require(:storage).permit(:image,:name).merge(user_id: current_user.id)
   end
 end
