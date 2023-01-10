f2aef203593e064d7f668c7b294577014259d710
soya0128 <bori.rhythem2545@gmail.com>
2023-01-10 23:51:22 +0900

    roomモデル、messageモデルの単体テストコードの記述

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :message do
-    
+    message_content {Faker::Lorem.sentence}
+    association :user
+    association :room
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/present_box.png'), filename: 'present_box.png')
+    end
   end
 end
