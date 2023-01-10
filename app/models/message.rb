4f8039e73c36885ae1a2e3935d670ccc6102bbde
soya0128 <bori.rhythem2545@gmail.com>
2023-01-10 23:50:49 +0900

    roomモデル、messageモデルの編集

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,11 @@
 class Message < ApplicationRecord
-  
+  validates :message_content, presence: true, unless: :was_attached?
+
   belongs_to :user
   belongs_to :room
+  has_one_attached :image
+
+  def was_attached?
+    self.image.attached?
+  end
 end
