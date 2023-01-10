6af41a5f2065275b10abffbcde168ea785fa6b02
soya0128 <bori.rhythem2545@gmail.com>
2023-01-10 23:50:22 +0900

    roomsコントローラー、messagesコントローラーの編集

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,29 @@
+class RoomsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to  room_messages_path(@room)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    room = Room.find(params[:id])
+    room.destroy
+    redirect_to rooms_path
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:room_name, user_ids: [])
+  end
+end
