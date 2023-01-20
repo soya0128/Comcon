4177150c0ffb85a0714b0cbc9b53ec3aaddf7214
soya0128 <bori.rhythem2545@gmail.com>
2023-01-20 17:08:57 +0900

    paymentsコントローラーの編集

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,2 +1,36 @@
 class PaymentsController < ApplicationController
+  def index
+    @user = User.find(current_user.id)
+    @payments = Payment.includes(:user).order("created_at DESC").where(:user_id == current_user.id)
+  end
+
+  def new
+    @payment = Payment.new
+  end
+
+  def create
+    @payment = Payment.new(payment_params)
+    if @payment.valid?
+      pay_by_card
+      @payment.save
+      return redirect_to payments_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def payment_params
+    params.require(:payment).permit(:price).merge(user_id: current_user.id, token: params[:token])
+  end
+
+  def pay_by_card
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: payment_params[:price],
+      card: payment_params[:token],
+      currency: 'jpy'
+    )
+  end
 end
